f8315d11e745952ec541f1ffb4c5f9b92bf230b3
SudhangsuShekharBairagi <bairagisudhangsu02@gmail.com>
2026-06-20 21:24:47 +0530

    Add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 ﻿import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Style from './Navbar.module.css';
+import logo from "../assets/logo.jpeg"
 const Navbar = () => {
   return (
 
     <div className={Style.nav}>
-      <img className={Style.navLogo} src="/logo.jpeg" alt="" />
+      <img className={Style.navLogo} src={logo} alt="Logo" />
 
       <nav  >
         <NavLink to={"/movie"} className={Style.navLink}>Movies</NavLink>
