bc311f3f3b1c6528f5f755982f81455075423418
SudhangsuShekharBairagi <bairagisudhangsu02@gmail.com>
2026-07-12 17:12:52 +0530

    Add Scroll Restoration

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import Footer from '../pages/Footer'
 
 const NavLayout = () => {
     return (
         <>
             <Navbar />
+            <ScrollRestoration />
             <Outlet />
             <Footer />
 
